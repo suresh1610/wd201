3c8dff78a20ca4b6182c3f97e25551abefdf32d8
suresh <sureshkanthasamy39@gmail.com>
2023-02-26 16:03:25 +0530

    updated files

--- http-server/index.js
+++ http-server/index.js
@@ -3,47 +3,49 @@ const fs = require("fs");
 const args = require("minimist")(process.argv);
 const port = args.port;
 
-let homeContent = "";
-let projectContent = "";
-let registrationContent = "";
+let homepage = "";
+let projectpage = "";
+let registrationpage = "";
 
 fs.readFile("home.html", (err, home) => {
   if (err) {
-    throw err;S
+    throw err;
   }
-  homeContent = home;
+  homepage = home;
 });
 
 fs.readFile("project.html", (err, project) => {
   if (err) {
     throw err;
   }
-  projectContent = project;
+  projectpage = project;
 });
 
 fs.readFile("registration.html", (err, registration) => {
-    if (err) {
-      throw err;
-    }
-    registrationContent = registration;
-  });
-http
-  .createServer((request, response) => {
+  if (err) {
+    throw err;
+  }
+  registrationpage = registration;
+});
+
+http.createServer((request, response) => {
     let url = request.url;
     response.writeHeader(200, { "Content-Type": "text/html" });
     switch (url) {
-      case "/project.html":
-        response.write(projectContent);
+      case "project":
+        response.write(projectpage);
         response.end();
         break;
-        case "/registration.html":
-        response.write(registrationContent);
+
+      case "registration":
+        response.write(registrationpage);
         response.end();
         break;
+
       default:
-        response.write(homeContent);
+        response.write(homepage);
         response.end();
         break;
     }
   })
-  .listen(5000);
+  .listen(port);
